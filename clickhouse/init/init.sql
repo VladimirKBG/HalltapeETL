1207d660aa772ab69752e1d50f5c00cbb23da0a5
Nikita <trustisreach@gmail.com>
2025-08-11 09:53:42 +0000

    adjust init

--- clickhouse/init/init.sql
+++ clickhouse/init/init.sql
@@ -14,3 +14,50 @@ CREATE TABLE IF NOT EXISTS default.enriched_earthquakes (
 ) ENGINE = MergeTree
 PARTITION BY toYYYYMM(load_date)
 ORDER BY (load_date, region);
+
+
+CREATE TABLE default.earth_quake_agg
+(
+
+    `place_hash` String,
+
+    `count` Int32
+)
+ENGINE = SummingMergeTree
+ORDER BY place_hash
+SETTINGS index_granularity = 8192;
+
+CREATE TABLE default.earth_quake_full
+(
+
+    `id` String,
+
+    `ts` DateTime,
+
+    `load_date` Date,
+
+    `magnitude` Float64,
+
+    `felt` Int64,
+
+    `tsunami` Int64,
+
+    `url` String,
+
+    `longitude` Float64,
+
+    `latitude` Float64,
+
+    `depth` Float64,
+
+    `place_hash` String,
+
+    `updated_at` DateTime,
+
+    `load_to_ch_utc` DateTime DEFAULT now()
+)
+ENGINE = MergeTree
+PARTITION BY toYYYYMM(updated_at)
+ORDER BY (load_date,
+ id)
+SETTINGS index_granularity = 8192;
